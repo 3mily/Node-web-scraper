170587465cc20bdec1fa966cbb2be0d05646b092
3mily <emrcraig@gmail.com>
2015-02-18 05:40:14 +0000

    Able to scrape website & output to console

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ var cheerio = require('cheerio');
 
 request("http://substack.net/images/", function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log("success");
-    // console.log(body); => all the HTML for the page
+    console.log("success!");
     var $ = cheerio.load(body);
-    $('').each(function(i, element){
-      console.log(element);
+
+    // for each tr do ~~
+    $('tr').each(function(i, element){
+      var url = "http://substack.net/images" + element.children[2].children[0].attribs.href;
+      console.log("-Url: " + url);
+      var permission = element.children[0].children[0].children[0].data
+      console.log("-Permission: " + permission)
+
+  // if (url.split(".")[1].match(/\./)){
+      if (url.match(/\./)){
+        var fileType = url.split(".")[2];
+      } else {
+        var fileType = "directory"
+      }
+      console.log("-File type: " + fileType);
+      console.log("=====");
+
     });
-  }
+  };
 });
+
+console.log("--------- loading... ---------");
+
+
